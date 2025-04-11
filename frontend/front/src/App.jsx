3d087bf5f1ab7e7d4c3ca6724e4f35d89eec7457
anirudhreddy000 <mvanirudhreddy@gmail.com>
2025-04-11 06:42:50 +0530

    done

--- frontend/front/src/App.jsx
+++ frontend/front/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UploadPage from "./UploadPage";
-import PredictionPage from "./PredictionPage";
+//import PredictionPage from "./PredictionPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<UploadPage />} />
-        <Route path="/predict" element={<PredictionPage />} />
+        
       </Routes>
     </Router>
   );
